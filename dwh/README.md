c16349cb25d04270dc9764e65da7675c21bc3d2e
rwm <raphael@majeed.de>
2015-05-21 18:07:30 +0200

    Update auf i2b2-Version 1.7.05

---
+++ dwh/README.md
@@ -0,0 +1,22 @@
+Virtuelle Maschine fuer Data Warehouse
+======================================
+
+Ordner packages/
+----------------
+
+Im Ordner 'packages' sollten ZIP Archive liegen, die
+für die Installation benötigt werden.
+
+Von den offiziellen Webseiten von i2b2 https://www.i2b2.org/software/
+müssen die folgenden Pakete heruntergeladen werden, da sie aus 
+Lizenzgründen nicht automatisch heruntergeladen werden können:
+* i2b2core-src-1705.zip
+* i2b2createdb-1705.zip
+* i2b2webclient-1705.zip
+
+Zusätzlich werden Pakete die folgenden Pakete bei Bedarf
+automatisch heruntergeladen:
+
+* Axis2 1.6.2
+* Apache Ant 1.8.2
+* JBoss AS 7.1.1 Final
--- dwh/bootstrap.sh
+++ dwh/bootstrap.sh
@@ -8,19 +8,27 @@ apt-get install -y wget curl subversion dos2unix dialog
 
 cd /opt
 # Download i2b2 Wizard
-svn checkout --revision 295 http://community.i2b2.org/repos/i2b2/trunk/related/i2b2Wizard/trunk
+svn checkout http://community.i2b2.org/repos/i2b2/trunk/related/i2b2Wizard/trunk
 mv trunk i2b2Wizard
 cd i2b2Wizard
+
+# remove svn directories
+find . -name .svn -type d -print0 | xargs -0 -n1 rm -rf
+
+# convert scripts
 find . -name "*.sh" -type f -exec dos2unix {} \;
 chmod +x wizard.sh
 
-cp scripts/os_versions/Ubuntu_14.04.2_LTS.sh scripts/os_versions/Debian_7.8.sh
+# link packages
+rmdir packages
+ln -s $install_root/packages packages
+
+
+cp scripts/os_versions/Ubuntu_14.04.sh scripts/os_versions/Debian_7.8.sh
 
 # apply patches
 patch -p1 < $install_root/i2b2Wizard.patch
 
-# copy sources
-cp -v $install_root/packages/*.zip ./packages/
 
 # additional dependencies for i2b2-wizard
 apt-get install -y aptitude libcurl3 libapache2-mod-php5 php5-curl perl sed bc postgresql
@@ -33,6 +41,8 @@ apt-get install -y openjdk-7-jdk
 # debian7.8 has ant package with required version 1.8.2, no need to compile ant from source
 
 # enable remote access to postgres
-/vagrant/postgres-remote-access.sh
+cp /vagrant/postgres-remote-access.sh /opt/
+dos2unix /opt/postgres-remote-access.sh
+/opt/postgres-remote-access.sh
 
 # TODO: command line installation with i2b2-wizard
