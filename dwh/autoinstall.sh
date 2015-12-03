eb9de4565cbfd932735bcb907bfe3a56b75a31c1
Tingyan Xu <tingyan.xu@uni-oldenburg.de>
2015-12-03 13:34:11 +0100

    autoinstall fix

--- dwh/autoinstall.sh
+++ dwh/autoinstall.sh
@@ -3,6 +3,9 @@
 cd /vagrant/i2b2_install
 pwd
 echo install jboss
+if [ ! -d "../logs" ]; then 
+    mkdir ../logs
+fi
 ./install_jboss.sh > ../logs/install_jboss.log
 
 cd /opt/jboss-as-7.1.1.Final/
