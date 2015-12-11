f92167b1bb2c20846d9570274e2922397b4a3af9
Tingyan Xu <tingyan.xu@uni-oldenburg.de>
2015-12-11 15:43:49 +0100

    renamed pm db

--- dwh/i2b2_install/db_pm/delete.sh
+++ dwh/i2b2_install/db_pm/delete.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 
-. init.sh
+. ../install.conf
 
 # go to folder
-cd $DATA_HOME/pm_db
+cd $DATA_HOME/db_pm
 
 $ANT_HOME/bin/ant -f change_build.xml drop_pm 2> $LOG_DIR/pm_del.err.log > $LOG_DIR/pm_del.log
 
