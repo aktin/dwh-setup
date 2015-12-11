49c61ec9fbd2414143235800aa11b166fc2aaa8a
Tingyan Xu <tingyan.xu@uni-oldenburg.de>
2015-12-11 15:41:55 +0100

    meta_db umbenannt

--- dwh/i2b2_install/db_meta/delete.sh
+++ dwh/i2b2_install/db_meta/delete.sh
@@ -3,7 +3,7 @@
 . ../install.conf
 
 # go to folder
-cd $DATA_HOME/meta_db
+cd $DATA_HOME/db_meta
 
 $ANT_HOME/bin/ant -f change_build.xml drop_meta 2> $LOG_DIR/meta_boston_del.err.log > $LOG_DIR/meta_boston_del.log
 
