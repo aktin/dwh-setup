f87b171631676c5474ce9c8b7d110f37ca16778f
Tingyan Xu <tingyan.xu@uni-oldenburg.de>
2015-12-11 15:44:14 +0100

    renamed work db

--- dwh/i2b2_install/db_work/delete.sh
+++ dwh/i2b2_install/db_work/delete.sh
@@ -3,7 +3,7 @@
 . ../install.conf
 
 # go to folder
-cd $DATA_HOME/work_db
+cd $DATA_HOME/db_work
 
 $ANT_HOME/bin/ant -f change_build.xml drop_work 2> $LOG_DIR/wrok_boston_del.err.log > $LOG_DIR/wrok_boston_del.log
 
