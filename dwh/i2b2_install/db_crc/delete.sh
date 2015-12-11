fa9ef68b01af886d36ced7a1ba3f2e3f3667bd4f
Tingyan Xu <tingyan.xu@uni-oldenburg.de>
2015-12-11 15:41:23 +0100

    crc_db umbenannt

--- dwh/i2b2_install/db_crc/delete.sh
+++ dwh/i2b2_install/db_crc/delete.sh
@@ -3,7 +3,7 @@
 . ../install.conf
 
 # go to folder
-cd $DATA_HOME/crc_db
+cd $DATA_HOME/db_crc
 
 $ANT_HOME/bin/ant -f change_build.xml drop_crc 2> $LOG_DIR/demo_crc_boston_del.err.log > $LOG_DIR/demo_crc_boston_del.log
 
