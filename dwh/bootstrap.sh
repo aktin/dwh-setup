2311c35452829e5dbbf6aab9acc1ae39779253db
Tingyan Xu <tingyan.xu@uni-oldenburg.de>
2015-12-16 19:08:58 +0100

    updated bootstrap

--- dwh/bootstrap.sh
+++ dwh/bootstrap.sh
@@ -6,52 +6,36 @@ install_root=/vagrant
 apt-get update
 apt-get install -y wget curl subversion dos2unix dialog
 
+: <<'END'
+# i2b2Wizard
 cd /opt
 # Download i2b2 Wizard
 # TODO include current revision number in checkout
-# svn checkout http://community.i2b2.org/repos/i2b2/trunk/related/i2b2Wizard/trunk --revision 331
-# mv trunk i2b2Wizard
-# cd i2b2Wizard
+svn checkout http://community.i2b2.org/repos/i2b2/trunk/related/i2b2Wizard/trunk --revision 343
+mv trunk i2b2Wizard
+cd i2b2Wizard
 
 # remove svn directories
-# find . -name .svn -type d -print0 | xargs -0 -n1 rm -rf
+ find . -name .svn -type d -print0 | xargs -0 -n1 rm -rf
 
 # convert scripts
-# find . -name "*.sh" -type f -exec dos2unix {} \;
-# chmod +x wizard.sh
+ find . -name "*.sh" -type f -exec dos2unix {} \;
+ chmod +x wizard.sh
 
 # link packages
-# rmdir packages
-# ln -s $install_root/packages .
+ rmdir packages
+ ln -s $install_root/packages .
+END
 
-
-# additional dependencies for i2b2-wizard
-apt-get install -y aptitude libcurl3 libapache2-mod-php5 php5-curl perl sed bc postgresql-9.1
-# jre-headless not sufficient, need full jdk
-#apt-get install -y openjdk-7-jre-headless
-#apt-get install -y openjdk-8-jdk
-
-#apt-get -y dist-upgrade
-
-# debian7.8 has ant package with required version 1.8.2, no need to compile ant from source
-
-# enable remote access to postgres
-cp $install_root/postgres-remote-access.sh /opt/
-dos2unix /opt/postgres-remote-access.sh
-/opt/postgres-remote-access.sh
-
-# TODO: command line installation with i2b2-wizard
-#cp $install_root/autoinstall.sh /opt/
-#dos2unix /opt/autoinstall.sh
-#cp $install_root/autoinstall.wizard.conf /opt/i2b2Wizard/config/wizard.conf
-#dos2unix /opt/i2b2Wizard/config/wizard.conf
-
-#/opt/autoinstall.sh
+ifconfig
 
 # create postgres databases for i2b2
-cd $install_root
-dos2unix /vagrant/i2b2_install/install_jboss.sh
-dos2unix /vagrant/i2b2_install/i2b2_db_full_install.sh
+# cd $install_root
 dos2unix /vagrant/autoinstall.sh
-/vagrant/autoinstall.sh
 
+LOG_DIR=/vagrant/logs
+if [ ! -d "$LOG_DIR" ]; then 
+    mkdir $LOG_DIR
+fi
+
+/vagrant/autoinstall.sh > $LOG_DIR/autoinstall.log 2> $LOG_DIR/autoinstall.err.log
