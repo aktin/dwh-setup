b75b23394f674bc8ad06230b8e2e3985e2fe3812
R.W.Majeed <raphael@majeed.de>
2016-01-01 18:26:47 +0100

    trust local networks for postgres connections (debug)

--- dwh/postgres-remote-access.sh
+++ dwh/postgres-remote-access.sh
@@ -13,7 +13,8 @@ if [ ! -f $FILE.orig ] ; then
 fi
 # trust all remote connections (for development)
 cat $FILE.orig > $FILE
-echo 'host all all 10.0.0.2/8 trust' >> $FILE
+echo 'host all all 192.168.0.0/16 trust' >> $FILE
+echo 'host all all 10.0.0.0/8 trust' >> $FILE
 # XXX maybe use md5 instead of trust for more security
 
 
