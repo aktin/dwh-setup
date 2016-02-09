68914adfa606f9bc3606d738c66f9dfa79f8767d
tx <tx>
2016-02-09 15:58:25 +0100

    update loggin

--- dwh/logging.sh
+++ dwh/logging.sh
@@ -5,10 +5,12 @@ logdir=/vagrant/logs/logs_$d
 
 if [ ! -d "$logdir" ]; then 
     mkdir $logdir
+    mkdir $logdir/apache2_server
+    mkdir $logdir/postgres
 fi
 cd $logdir
 cp /var/log/wildfly/console.log wildfly_console.log
 cp /opt/wildfly-9.0.2.Final/standalone/log/server.log wildfly_standalone.log
-cp -r /var/log/apache2 apache2_server/
+cp -r /var/log/apache2/* apache2_server
 cp /var/log/syslog syslog
-cp -r /var/log/postgresql postgres
+cp -r /var/log/postgresql/* postgres
