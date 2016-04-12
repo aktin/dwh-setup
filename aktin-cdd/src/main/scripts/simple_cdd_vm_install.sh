3d0fb07299b9a83141925253994303be22dc9279
Tingyan Xu <tingyan.xu@uni-oldenburg.de>
2016-04-12 16:47:10 +0200

    added assembly to aktin-cdd

--- aktin-cdd/src/main/scripts/simple_cdd_vm_install.sh
+++ aktin-cdd/src/main/scripts/simple_cdd_vm_install.sh
@@ -11,3 +11,10 @@ CDD_PATH=
 apt-get update
 apt-get install -y sudo wget curl dos2unix unzip sed bc simple-cdd 
 
+# su - vagrant
+
+# mkdir -P aktin-cdd/profiles
+
+# cp scdd-profiles/aktin.* aktin-cdd/profiles/
+
+# mkdir -P aktin-cdd/local_extras
