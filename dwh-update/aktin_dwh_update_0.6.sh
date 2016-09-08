564c1a89e8d8441c3df8cadb6f972ff0887e460c
Tingyan Xu <tingyan.xu@uni-oldenburg.de>
2016-09-08 10:58:52 +0200

    gekapseltes sql löschen und ausblenden von smtp

--- dwh-update/aktin_dwh_update_0.6.sh
+++ dwh-update/aktin_dwh_update_0.6.sh
@@ -11,16 +11,23 @@ WILDFLY_HOME=/opt/wildfly-9.0.2.Final
 echo "Undeploying 0.5 DWH EAR file"
 if [ -f "$WILDFLY_HOME/standalone/deployments/dwh-j2ee-0.5-SNAPSHOT.ear" ] && [ ! -f "$WILDFLY_HOME/standalone/deployments/dwh-j2ee-0.5-SNAPSHOT.ear.undeployed" ]; then 
 	$WILDFLY_HOME/bin/jboss-cli.sh -c --command="undeploy --name=dwh-j2ee-0.5-SNAPSHOT.ear"
+
+	echo ""
+	echo "++++++++++++++++++++++"
+	echo ""
+
+	# STEP 4 - Execute Database Scripts
+	echo "reset Postgres CRC database"
+	. ./scripts/postgres_cleanse.sh
+	echo ""
 else 
-	echo "Undeployment übersprungen, da keine alte Version gefunden oder bereits undeployed"
+	echo "Undeployment und SQL Skript übersprungen, da keine alte Version gefunden oder bereits undeployed"
 fi
 echo ""
 echo "++++++++++++++++++++++"
 echo ""
 echo "++++++++++++++++++++++"
 echo ""
-
-
 # STEP 2 - Deploy new Server-EAR
 echo "Deploying 0.6 DWH EAR file"
 if [ ! -f "$WILDFLY_HOME/standalone/deployments/dwh-j2ee-0.6-SNAPSHOT.ear" ]; then 
@@ -28,24 +35,11 @@ if [ ! -f "$WILDFLY_HOME/standalone/deployments/dwh-j2ee-0.6-SNAPSHOT.ear" ]; th
 else 
 	echo "Deployment übersprungen, da die Datei bereits in $WILDFLY_HOME/standalone/deployments/ vorhanden ist."
 fi
-echo ""
-echo "++++++++++++++++++++++"
-echo ""
-echo "++++++++++++++++++++++"
-echo ""
-
-
-# STEP 3 - create new mail service
-echo "SMTP Configuration"
-. ./scripts/smtp_setup_config.sh
-echo ""
-echo "++++++++++++++++++++++"
-echo ""
-echo "++++++++++++++++++++++"
-echo ""
-
-
-# STEP 4 - Execute Database Scripts
-echo "reset Postgres CRC database"
-. ./scripts/postgres_cleanse.sh
-echo ""
+# echo ""
+# echo "++++++++++++++++++++++"
+# echo ""
+# echo "++++++++++++++++++++++"
+# echo ""
+# # STEP 3 - create new mail service
+# echo "SMTP Configuration"
+# . ./scripts/smtp_setup_config.sh
