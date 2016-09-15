4d0550e46ec086e406c9ee7fc1e90fd0a5584161
R.W.Majeed <raphael@majeed.de>
2016-09-15 23:35:23 +0200

    tee for both logging and output. deployment versions as variables.

--- dwh-update/src/main/scripts/aktin_dwh_update_0.6.sh
+++ dwh-update/src/main/scripts/aktin_dwh_update_0.6.sh
@@ -5,15 +5,18 @@ SCRIPT=$(readlink -f "$0")
 install_root=$(dirname "$SCRIPT")
 WILDFLY_HOME=/opt/wildfly-9.0.2.Final
 
+OLD_EAR=dwh-j2ee-0.5-SNAPSHOT.ear
+NEW_EAR=dwh-j2ee-0.6-SNAPSHOT.ear
+
 LOGFILE=$install_root/update.log
 touch $LOGFILE
 
 # STEP 1 - Undeploy old Server-EAR via CLI
 # if older version exists, then undeploy it
 echo "Undeploying 0.5 DWH EAR file"
-if [ -f "$WILDFLY_HOME/standalone/deployments/dwh-j2ee-0.5-SNAPSHOT.ear" ] && [ ! -f "$WILDFLY_HOME/standalone/deployments/dwh-j2ee-0.5-SNAPSHOT.ear.undeployed" ]; then 
-	echo "STEP 1"  >> $LOGFILE
-	$WILDFLY_HOME/bin/jboss-cli.sh -c --command="undeploy --name=dwh-j2ee-0.5-SNAPSHOT.ear" >> $LOGFILE
+if [ -f "$WILDFLY_HOME/standalone/deployments/$OLD_EAR" ] && [ ! -f "$WILDFLY_HOME/standalone/deployments/$OLD_EAR.undeployed" ]; then 
+	echo "STEP 1"  | tee -a $LOGFILE
+	$WILDFLY_HOME/bin/jboss-cli.sh -c --command="undeploy --name=$OLD_EAR" | tee -a $LOGFILE
 
 	echo ""
 	echo "++++++++++++++++++++++"
@@ -21,16 +24,16 @@ if [ -f "$WILDFLY_HOME/standalone/deployments/dwh-j2ee-0.5-SNAPSHOT.ear" ] && [ 
 
 	# STEP 1.1 - Execute Database Scripts
 	echo "STEP 1.1 Reset i2b2 CRC database"
-	. ./postgres_cleanse.sh >> $LOGFILE
+	. ./postgres_cleanse.sh | tee -a $LOGFILE
 	echo ""
 
 	# STEP 1.2 - Ontologien
 	echo "STEP 1.2 update i2b2 Ontology Cell Data"
-	. ./postgres_update_ontology.sh >> $LOGFILE
+	. ./postgres_update_ontology.sh | tee -a $LOGFILE
 	echo ""
 
 else 
-	echo "STEP 1 not deployed" >> $LOGFILE
+	echo "STEP 1 not deployed" | tee -a $LOGFILE
 	echo "Undeployment und SQL Skript übersprungen, da keine alte Version gefunden oder bereits undeployed"
 fi
 echo ""
@@ -38,11 +41,11 @@ echo "++++++++++++++++++++++"
 echo ""
 # STEP 2 - Deploy new Server-EAR
 echo "Deploying 0.6 DWH EAR file"
-if [ ! -f "$WILDFLY_HOME/standalone/deployments/dwh-j2ee-0.6-SNAPSHOT.ear" ]; then 
-	cp $install_root/packages/dwh-j2ee-0.6-SNAPSHOT.ear $WILDFLY_HOME/standalone/deployments/dwh-j2ee-0.6-SNAPSHOT.ear
-	echo "STEP 2 file copied " >> $LOGFILE
+if [ ! -f "$WILDFLY_HOME/standalone/deployments/$NEW_EAR" ]; then 
+	cp $install_root/packages/$NEW_EAR $WILDFLY_HOME/standalone/deployments/
+	echo "STEP 2 file copied " | tee -a $LOGFILE
 else 
-	echo "STEP 2 not copied" >> $LOGFILE
+	echo "STEP 2 not copied" | tee -a $LOGFILE
 	echo "Deployment übersprungen, da die Datei bereits in $WILDFLY_HOME/standalone/deployments/ vorhanden ist."
 fi
 # echo ""
@@ -60,10 +63,10 @@ echo "Removing standard data from Login Form"
 i2b2_WEBDIR=/var/webroot/webclient
 if [ ! -f $i2b2_WEBDIR/js-i2b2/cells/PM/PM_misc.js.orig ]; then 
 	cp $i2b2_WEBDIR/js-i2b2/cells/PM/PM_misc.js $i2b2_WEBDIR/js-i2b2/cells/PM/PM_misc.js.orig
-	echo "STEP 4 file backed up" >> $LOGFILE
+	echo "STEP 4 file backed up" | tee -a $LOGFILE
 fi
 sed -i "s/name=\"uname\" id=\"loginusr\" value=\"demo\"/name=\"uname\" id=\"loginusr\" value=\"\"/g; s/name=\"pword\" id=\"loginpass\" value=\"demouser\"/name=\"pword\" id=\"loginpass\" value=\"\"/g" $i2b2_WEBDIR/js-i2b2/cells/PM/PM_misc.js
-echo "STEP 4 replaced" >> $LOGFILE
+echo "STEP 4 replaced" | tee -a $LOGFILE
 
 echo ""
 echo "++++++++++++++++++++++"
--- dwh-update/src/main/scripts/postgres_update_ontology.sh
+++ dwh-update/src/main/scripts/postgres_update_ontology.sh
@@ -7,7 +7,7 @@ echo "update ontologies"
 unzip packages/cda-ontology-0.6-SNAPSHOT.jar -d $CDATMPDIR
 chmod 777 -R $CDATMPDIR
 touch update_sql.log
-echo "update metadata " >> update_sql.log
-su - postgres bash -c "psql -d i2b2 -f $CDATMPDIR/sql/meta.sql" >> update_sql.log
-echo "update crcdata " >> update_sql.log
-su - postgres bash -c "psql -d i2b2 -f $CDATMPDIR/sql/data.sql" >> update_sql.log
+echo "update metadata " | tee -a update_sql.log
+su - postgres bash -c "psql -d i2b2 -f $CDATMPDIR/sql/meta.sql" | tee -a update_sql.log
+echo "update crcdata " | tee -a update_sql.log
+su - postgres bash -c "psql -d i2b2 -f $CDATMPDIR/sql/data.sql" | tee -a update_sql.log
