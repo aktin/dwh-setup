a191d89af778b1bcd54f1d918429406aa2f36d1f
Tingyan Xu <tingyan.xu@uni-oldenburg.de>
2016-09-19 16:47:29 +0200

    pipe stderr to stdout and removed 0.6 from package name

--- dwh-update/src/main/scripts/aktin_dwh_update_0.6.sh
+++ dwh-update/src/main/scripts/aktin_dwh_update_0.6.sh
@@ -15,8 +15,8 @@ touch $LOGFILE
 # if older version exists, then undeploy it
 echo "Undeploying old DWH EAR file"
 if [ -f "$WILDFLY_HOME/standalone/deployments/$OLD_EAR" ] && [ ! -f "$WILDFLY_HOME/standalone/deployments/$OLD_EAR.undeployed" ]; then 
-	echo "STEP 1"  | tee -a $LOGFILE
-	$WILDFLY_HOME/bin/jboss-cli.sh -c --command="undeploy --name=$OLD_EAR" | tee -a $LOGFILE
+	echo "STEP 1"  2>&1 | tee -a $LOGFILE
+	$WILDFLY_HOME/bin/jboss-cli.sh -c --command="undeploy --name=$OLD_EAR" 2>&1 | tee -a $LOGFILE
 
 	echo ""
 	echo "++++++++++++++++++++++"
@@ -24,16 +24,16 @@ if [ -f "$WILDFLY_HOME/standalone/deployments/$OLD_EAR" ] && [ ! -f "$WILDFLY_HO
 
 	# STEP 1.1 - Execute Database Scripts
 	echo "STEP 1.1 Reset i2b2 CRC database"
-	. ./postgres_cleanse.sh | tee -a $LOGFILE
+	. ./postgres_cleanse.sh 2>&1 | tee -a $LOGFILE
 	echo ""
 
 	# STEP 1.2 - Ontologien
 	echo "STEP 1.2 update i2b2 Ontology Cell Data"
-	. ./postgres_update_ontology.sh | tee -a $LOGFILE
+	. ./postgres_update_ontology.sh 2>&1 | tee -a $LOGFILE
 	echo ""
 
 else 
-	echo "STEP 1 not deployed" | tee -a $LOGFILE
+	echo "STEP 1 not deployed" 2>&1 | tee -a $LOGFILE
 	echo "Undeployment und SQL Skript übersprungen, da keine alte Version gefunden oder bereits undeployed"
 fi
 echo ""
@@ -43,9 +43,9 @@ echo ""
 echo "Deploying new DWH EAR file"
 if [ ! -f "$WILDFLY_HOME/standalone/deployments/$NEW_EAR" ]; then 
 	cp $install_root/packages/$NEW_EAR $WILDFLY_HOME/standalone/deployments/
-	echo "STEP 2 file copied " | tee -a $LOGFILE
+	echo "STEP 2 file copied " 2>&1 | tee -a $LOGFILE
 else 
-	echo "STEP 2 not copied" | tee -a $LOGFILE
+	echo "STEP 2 not copied" 2>&1 | tee -a $LOGFILE
 	echo "Deployment übersprungen, da die Datei bereits in $WILDFLY_HOME/standalone/deployments/ vorhanden ist."
 fi
 # echo ""
@@ -63,10 +63,10 @@ echo "Removing standard data from Login Form"
 i2b2_WEBDIR=/var/webroot/webclient
 if [ ! -f $i2b2_WEBDIR/js-i2b2/cells/PM/PM_misc.js.orig ]; then 
 	cp $i2b2_WEBDIR/js-i2b2/cells/PM/PM_misc.js $i2b2_WEBDIR/js-i2b2/cells/PM/PM_misc.js.orig
-	echo "STEP 4 file backed up" | tee -a $LOGFILE
+	echo "STEP 4 file backed up" 2>&1 | tee -a $LOGFILE
 fi
 sed -i "s/name=\"uname\" id=\"loginusr\" value=\"demo\"/name=\"uname\" id=\"loginusr\" value=\"\"/g; s/name=\"pword\" id=\"loginpass\" value=\"demouser\"/name=\"pword\" id=\"loginpass\" value=\"\"/g" $i2b2_WEBDIR/js-i2b2/cells/PM/PM_misc.js
-echo "STEP 4 replaced" | tee -a $LOGFILE
+echo "STEP 4 replaced" 2>&1 | tee -a $LOGFILE
 
 echo ""
 echo "++++++++++++++++++++++"
