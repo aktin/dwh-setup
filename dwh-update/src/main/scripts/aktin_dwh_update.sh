5244a52fcb4701366ee8872c621824af2c4c2a33
Tingyan Xu <tingyan.xu@uni-oldenburg.de>
2016-09-20 17:31:04 +0200

    logging erweitertt

--- dwh-update/src/main/scripts/aktin_dwh_update.sh
+++ dwh-update/src/main/scripts/aktin_dwh_update.sh
@@ -13,22 +13,22 @@ touch $LOGFILE
 
 # STEP 1 - Undeploy old Server-EAR via CLI
 # if older version exists, then undeploy it
-echo "Undeploying old DWH EAR file"
+echo "Undeploying old DWH EAR file" 2>&1 | tee -a $LOGFILE
 if [ -f "$WILDFLY_HOME/standalone/deployments/$OLD_EAR" ] && [ ! -f "$WILDFLY_HOME/standalone/deployments/$OLD_EAR.undeployed" ]; then 
 	echo "STEP 1"  2>&1 | tee -a $LOGFILE
 	$WILDFLY_HOME/bin/jboss-cli.sh -c --command="undeploy --name=$OLD_EAR" 2>&1 | tee -a $LOGFILE
 
 	echo ""
-	echo "++++++++++++++++++++++"
+	echo "++++++++++++++++++++++" 2>&1 | tee -a $LOGFILE
 	echo ""
 
 	# STEP 1.1 - Execute Database Scripts
-	echo "STEP 1.1 Reset i2b2 CRC database"
+	echo "STEP 1.1 Reset i2b2 CRC database" 2>&1 | tee -a $LOGFILE
 	. ./postgres_cleanse.sh 2>&1 | tee -a $LOGFILE
 	echo ""
 
 	# STEP 1.2 - Ontologien
-	echo "STEP 1.2 update i2b2 Ontology Cell Data"
+	echo "STEP 1.2 update i2b2 Ontology Cell Data" 2>&1 | tee -a $LOGFILE
 	. ./postgres_update_ontology.sh 2>&1 | tee -a $LOGFILE
 	echo ""
 
@@ -37,10 +37,10 @@ else
 	echo "Undeployment und SQL Skript übersprungen, da keine alte Version gefunden oder bereits undeployed"
 fi
 echo ""
-echo "++++++++++++++++++++++"
+echo "++++++++++++++++++++++" 2>&1 | tee -a $LOGFILE
 echo ""
 # STEP 2 - Deploy new Server-EAR
-echo "Deploying new DWH EAR file"
+echo "Deploying new DWH EAR file" 2>&1 | tee -a $LOGFILE
 if [ ! -f "$WILDFLY_HOME/standalone/deployments/$NEW_EAR" ]; then 
 	cp $install_root/packages/$NEW_EAR $WILDFLY_HOME/standalone/deployments/
 	echo "STEP 2 file copied " 2>&1 | tee -a $LOGFILE
@@ -56,17 +56,17 @@ fi
 # . ./smtp_setup_config.sh
 
 echo ""
-echo "++++++++++++++++++++++"
+echo "++++++++++++++++++++++"  2>&1 | tee -a $LOGFILE
 echo ""
 # STEP 4 - Remove data from Login form
-echo "Removing standard data from Login Form"
+echo "Removing standard data from Login Form" 2>&1 | tee -a $LOGFILE
 i2b2_WEBDIR=/var/webroot/webclient
 if [ ! -f $i2b2_WEBDIR/js-i2b2/cells/PM/PM_misc.js.orig ]; then 
 	cp $i2b2_WEBDIR/js-i2b2/cells/PM/PM_misc.js $i2b2_WEBDIR/js-i2b2/cells/PM/PM_misc.js.orig
-	echo "STEP 4 file backed up" 2>&1 | tee -a $LOGFILE
+	echo "STEP 4 Webclient PM file backed up" 2>&1 | tee -a $LOGFILE
 fi
 sed -i "s/name=\"uname\" id=\"loginusr\" value=\"demo\"/name=\"uname\" id=\"loginusr\" value=\"\"/g; s/name=\"pword\" id=\"loginpass\" value=\"demouser\"/name=\"pword\" id=\"loginpass\" value=\"\"/g" $i2b2_WEBDIR/js-i2b2/cells/PM/PM_misc.js
-echo "STEP 4 replaced" 2>&1 | tee -a $LOGFILE
+echo "STEP 4 values replaced" 2>&1 | tee -a $LOGFILE
 
 echo ""
 echo "++++++++++++++++++++++"
--- dwh-update/src/main/scripts/postgres_update_ontology.sh
+++ dwh-update/src/main/scripts/postgres_update_ontology.sh
@@ -2,17 +2,17 @@
 
 CDATMPDIR=/var/tmp/cda-ontology
 
-echo "update ontologies to ${ontology.version}"
+echo "update ontologies to ${ontology.version}" 2>&1 | tee -a update_sql.log
 # unzip the sql jar 
 unzip packages/cda-ontology-${ontology.version}.jar -d $CDATMPDIR
 chmod 777 -R $CDATMPDIR
 touch update_sql.log
 
 # call sql script files. no console output since spamming
-echo "update metadata " | tee -a update_sql.log
-su - postgres bash -c "psql -d i2b2 -f $CDATMPDIR/sql/meta.sql" >> update_sql.log
-echo "update crcdata " | tee -a update_sql.log
-su - postgres bash -c "psql -d i2b2 -f $CDATMPDIR/sql/data.sql" >> update_sql.log
+echo "update metadata " 2>&1 | tee -a update_sql.log
+su - postgres bash -c "psql -d i2b2 -f $CDATMPDIR/sql/meta.sql" 2>&1 >> update_sql.log
+echo "update crcdata " 2>&1 | tee -a update_sql.log
+su - postgres bash -c "psql -d i2b2 -f $CDATMPDIR/sql/data.sql" 2>&1 >> update_sql.log
 
 # remove temp directory
 rm -r $CDATMPDIR
