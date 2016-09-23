fe762609971e2ffecf2cfc21d791558ecf1fef65
Tingyan Xu <tingyan.xu@uni-oldenburg.de>
2016-09-23 13:25:22 +0200

    update ontology script: moved touch log to beginning of script

--- dwh-update/src/main/scripts/postgres_update_ontology.sh
+++ dwh-update/src/main/scripts/postgres_update_ontology.sh
@@ -2,11 +2,12 @@
 
 CDATMPDIR=/var/tmp/cda-ontology
 
+touch update_sql.log
+
 echo "update ontologies to ${org.aktin:cda-ontology:jar.version}" 2>&1 | tee -a update_sql.log
 # unzip the sql jar 
 unzip packages/cda-ontology-${org.aktin:cda-ontology:jar.version}.jar -d $CDATMPDIR
 chmod 777 -R $CDATMPDIR
-touch update_sql.log
 
 # call sql script files. no console output since spamming
 echo "update metadata " 2>&1 | tee -a update_sql.log
