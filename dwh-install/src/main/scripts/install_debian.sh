810c224eee52c1e7985654b4cc6d9c5c32d8b796
Tingyan Xu <tingyan.xu@uni-oldenburg.de>
2016-10-04 16:16:40 +0200

    dwh-installer release update: updated installer to 0.6

--- dwh-install/src/main/scripts/install_debian.sh
+++ dwh-install/src/main/scripts/install_debian.sh
@@ -101,6 +101,29 @@ echo "db.hive.id=i2b2demo" >> $buildfile
 echo ant scripts
 ant all 
 
+
+##### set up ontology
+
+
+CDATMPDIR=/var/tmp/cda-ontology
+mkdir $CDATMPDIR
+
+echo "update ontologies to ${org.aktin:cda-ontology:jar.version}" 2>&1 | tee -a update_sql.log
+# unzip the sql jar 
+unzip $PACKAGES/cda-ontology-${org.aktin:cda-ontology:jar.version}.jar -d $CDATMPDIR
+chmod 777 -R $CDATMPDIR
+touch update_sql.log
+
+# call sql script files. no console output since spamming
+echo "update metadata " 2>&1 | tee -a update_sql.log
+su - postgres bash -c "psql -d i2b2 -f $CDATMPDIR/sql/meta.sql" 2>&1 >> update_sql.log
+echo "update crcdata " 2>&1 | tee -a update_sql.log
+su - postgres bash -c "psql -d i2b2 -f $CDATMPDIR/sql/data.sql" 2>&1 >> update_sql.log
+
+# remove temp directory
+rm -r $CDATMPDIR
+
+
 ##### Set up wildfly
 # Create user
 adduser --system --group --disabled-login wildfly
