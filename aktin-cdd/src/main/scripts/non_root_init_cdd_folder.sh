53f08052c199a17d31ae4bb30f85321ac409ded7
Tingyan Xu <tingyan.xu@uni-oldenburg.de>
2016-10-05 11:52:01 +0200

    updated main pom and scripts for extract and download

--- aktin-cdd/src/main/scripts/non_root_init_cdd_folder.sh
+++ aktin-cdd/src/main/scripts/non_root_init_cdd_folder.sh
@@ -19,15 +19,15 @@ cp $MY_PATH/packages/dwh-install-*.tar.gz $CDD_PATH/local_extras/aktin-installer
 # download manual packages
 mkdir $MY_PATH/packages/packages 
 
-wget https://cloudstorage.uni-oldenburg.de/index.php/s/67OY8vh6BSZq56w/download -O $MY_PATH/packages/packages/axis2-1.6.3-war.zip
-wget https://cloudstorage.uni-oldenburg.de/index.php/s/0gRdGNsseGM5Gf9/download -O $MY_PATH/packages/packages/jboss-deployment-i2b2war-WEB-INF.zip
-wget https://cloudstorage.uni-oldenburg.de/index.php/s/x1caQ5R0KUSwwVZ/download -O $MY_PATH/packages/packages/i2b2createdb-1706.zip
-wget https://cloudstorage.uni-oldenburg.de/index.php/s/aWQcldqMgKlqxPC/download -O $MY_PATH/packages/packages/i2b2core-src-1706.zip
-wget https://cloudstorage.uni-oldenburg.de/index.php/s/aaLhFTfemXBKsQA/download -O $MY_PATH/packages/packages/i2b2webclient-1706.zip
-wget https://cloudstorage.uni-oldenburg.de/index.php/s/l8o6SM3Hueaxpen/download -O $MY_PATH/packages/packages/wildfly-9.0.2.Final.zip
-wget https://cloudstorage.uni-oldenburg.de/index.php/s/PkC9jJiUeZ8ILBQ/download -O $MY_PATH/packages/packages/postgresql-9.2-1002.jdbc4.jar
+wget -nv https://cloudstorage.uni-oldenburg.de/index.php/s/67OY8vh6BSZq56w/download -O $MY_PATH/packages/packages/axis2-1.6.3-war.zip
+wget -nv https://cloudstorage.uni-oldenburg.de/index.php/s/0gRdGNsseGM5Gf9/download -O $MY_PATH/packages/packages/jboss-deployment-i2b2war-WEB-INF.zip
+wget -nv https://cloudstorage.uni-oldenburg.de/index.php/s/x1caQ5R0KUSwwVZ/download -O $MY_PATH/packages/packages/i2b2createdb-1706.zip
+wget -nv https://cloudstorage.uni-oldenburg.de/index.php/s/aWQcldqMgKlqxPC/download -O $MY_PATH/packages/packages/i2b2core-src-1706.zip
+wget -nv https://cloudstorage.uni-oldenburg.de/index.php/s/aaLhFTfemXBKsQA/download -O $MY_PATH/packages/packages/i2b2webclient-1706.zip
+wget -nv https://cloudstorage.uni-oldenburg.de/index.php/s/l8o6SM3Hueaxpen/download -O $MY_PATH/packages/packages/wildfly-9.0.2.Final.zip
+wget -nv https://cloudstorage.uni-oldenburg.de/index.php/s/PkC9jJiUeZ8ILBQ/download -O $MY_PATH/packages/packages/postgresql-9.2-1002.jdbc4.jar
 
-tar czf $CDD_PATH/local_extras/packages.tar.gz $MY_PATH/packages/packages 
+tar czf $CDD_PATH/local_extras/packages.tar.gz $MY_PATH/packages/packages
 
 build-simple-cdd
 build-simple-cdd --profiles aktin 
