ab15efe36961ee49339f383c959980e12005aa85
R.W.Majeed <raphael@majeed.de>
2017-01-13 15:44:35 +0100

    TODO for login error

--- dwh-install/src/main/scripts/ds-aktin/postgres_db_script.sh
+++ dwh-install/src/main/scripts/ds-aktin/postgres_db_script.sh
@@ -5,3 +5,6 @@ psql -c "CREATE ROLE aktin with password 'aktin'" aktin
 psql -c "CREATE SCHEMA IF NOT EXISTS  aktin AUTHORIZATION aktin" aktin
 psql -c "GRANT ALL ON SCHEMA aktin to aktin" aktin
 
+## psql: FATAL:  role "aktin" is not permitted to log in
+## try: CREATE ROLE aktin with password 'aktin' LOGIN 
+## or ALTER ROLE "aktin" WITH LOGIN;
