a2e9c658064ea96637c84fe25182a1ecedbd4f81
Tingyan Xu <tingyan.xu@uni-oldenburg.de>
2017-01-25 16:40:52 +0100

    update script finish

--- dwh-update/src/main/scripts/0.7/aktin_dwh_update.sh
+++ dwh-update/src/main/scripts/0.7/aktin_dwh_update.sh
@@ -11,45 +11,47 @@ NEW_EAR=dwh-j2ee-0.7-SNAPSHOT.ear
 LOGFILE=$install_root/update.log
 touch $LOGFILE
 
+echo "AKTIN J2ee update to 0.7" 2>&1 | tee -a $LOGFILE
 
-
+echo ""
+echo "++++++++++++++++++++++" 2>&1 | tee -a $LOGFILE
+echo ""
 
 
 # STEP 1.1 - Create AKTIN Database in postgres for management uses
-
-echo "create AKTIN database with postgres user"
-su - postgres bash -c "$install_root/postgres_db_script.sh"
-
-
+# TODO check if the database or user exist. if not, then create. if yes. only update. Right now, creating while existing will return error, but continue with the code.
+echo "STEP 1.1 create AKTIN database with postgres user" 2>&1 | tee -a $LOGFILE
+su - postgres bash -c "$install_root/postgres_db_script.sh" 2>&1 | tee -a $LOGFILE
+echo "STEP 1.1 end: database created " 2>&1 | tee -a $LOGFILE
 
 # STEP 1.2 - Create Aktin Data source in wildfly
-echo "create AKTIN datasource in jboss"
-$WILDFLY_HOME/bin/jboss-cli.sh  --connect controller=127.0.0.1 --file=create_aktin_datassource.cli
+echo "STEP 1.2 create AKTIN datasource in jboss" 2>&1 | tee -a $LOGFILE
 
+existAktinDS=$( grep -c AktinDS $WILDFLY_HOME/standalone/configuration/standalone.xml)
+echo "- $existAktinDS occurences in Standalone.xml found" 2>&1 | tee -a $LOGFILE
+if ["$existAktinDS" -gt 0]
+then
+	$WILDFLY_HOME/bin/jboss-cli.sh  --connect controller=127.0.0.1 --commands="data-source remove --name=AktinDS,/subsystem=datasources:read-resource" 2>&1 | tee -a $LOGFILE
+	echo "STEP 1.2.1 end: removed older aktin datasource " 2>&1 | tee -a $LOGFILE
+fi
+$WILDFLY_HOME/bin/jboss-cli.sh  --connect controller=127.0.0.1 --file=create_aktin_datasource.cli 2>&1 | tee -a $LOGFILE
+echo "STEP 1.2.2 end: created aktin datasource" 2>&1 | tee -a $LOGFILE
+# $WILDFLY_HOME/bin/jboss-cli.sh  --connect controller=127.0.0.1 --commands="reload" 2>&1 | tee -a $LOGFILE
+# echo "STEP 1.2.3 end: reload" 2>&1 | tee -a $LOGFILE
 
-
-
-# # STEP 4 - Remove data from Login form
-# echo "Removing standard data from Login Form" 2>&1 | tee -a $LOGFILE
-# i2b2_WEBDIR=/var/webroot/webclient
-# if [ ! -f $i2b2_WEBDIR/js-i2b2/cells/PM/PM_misc.js.orig ]; then 
-# 	cp $i2b2_WEBDIR/js-i2b2/cells/PM/PM_misc.js $i2b2_WEBDIR/js-i2b2/cells/PM/PM_misc.js.orig
-# 	echo "STEP 4 Webclient PM file backed up" 2>&1 | tee -a $LOGFILE
-# fi
-# sed -i "s/name=\"uname\" id=\"loginusr\" value=\"demo\"/name=\"uname\" id=\"loginusr\" value=\"\"/g; s/name=\"pword\" id=\"loginpass\" value=\"demouser\"/name=\"pword\" id=\"loginpass\" value=\"\"/g" $i2b2_WEBDIR/js-i2b2/cells/PM/PM_misc.js
-# echo "STEP 4 values replaced" 2>&1 | tee -a $LOGFILE
-
-
+echo ""
+echo "++++++++++++++++++++++" 2>&1 | tee -a $LOGFILE
+echo ""
 
 # STEP 2 - Undeploy old Server-EAR via CLI
 # if older version exists, then undeploy it
-echo "Undeploying old DWH EAR file" 2>&1 | tee -a $LOGFILE
+echo "STEP 2 Undeploying old DWH EAR file" 2>&1 | tee -a $LOGFILE
 if [ -f "$WILDFLY_HOME/standalone/deployments/$OLD_EAR" ] && [ ! -f "$WILDFLY_HOME/standalone/deployments/$OLD_EAR.undeployed" ]; then 
-	echo "STEP 2"  2>&1 | tee -a $LOGFILE
 	$WILDFLY_HOME/bin/jboss-cli.sh -c --command="undeploy --name=$OLD_EAR" 2>&1 | tee -a $LOGFILE
+	echo "STEP 2 end: undeployed"  2>&1 | tee -a $LOGFILE
 else 
-	echo "STEP 2 not deployed" 2>&1 | tee -a $LOGFILE
-	echo "Undeployment, da keine alte Version gefunden oder bereits undeployed"
+	# Keine Undeployment, da keine alte Version gefunden oder bereits undeployed
+	echo "STEP 2 fail: not deployed" 2>&1 | tee -a $LOGFILE
 fi
 
 # clean up older ears
@@ -59,21 +61,14 @@ echo ""
 echo "++++++++++++++++++++++" 2>&1 | tee -a $LOGFILE
 echo ""
 
-
-
-
-
-
-
-
 # STEP 3 - copy new properties to configurations
-echo "Deploying new DWH EAR file" 2>&1 | tee -a $LOGFILE
+echo "STEP 3 Deploying new DWH EAR file" 2>&1 | tee -a $LOGFILE
 if [ ! -f "$WILDFLY_HOME/standalone/configuration/aktin.properties" ]; then 
 	cp $install_root/packages/aktin.properties $WILDFLY_HOME/standalone//configuration/aktin.properties 2>&1 | tee -a $LOGFILE
-	echo "STEP 3 properties file copied " 2>&1 | tee -a $LOGFILE
+	echo "STEP 3 end: properties file copied " 2>&1 | tee -a $LOGFILE
 else 
-	echo "STEP 3 properties not copied" 2>&1 | tee -a $LOGFILE
-	echo "Kopieren abgebrochen, da die Datei aktin.properties bereits in $WILDFLY_HOME/standalone/configuration/ vorhanden ist."
+	#Kopieren abgebrochen, da die Datei aktin.properties bereits in $WILDFLY_HOME/standalone/configuration/ vorhanden ist.
+	echo "STEP 3 fail: properties not copied" 2>&1 | tee -a $LOGFILE
 fi
 
 echo ""
@@ -82,25 +77,27 @@ echo ""
 
 
 # STEP 4 - Deploy new Server-EAR
-echo "Deploying new DWH EAR file" 2>&1 | tee -a $LOGFILE
+echo "STEP 4 Deploying new DWH EAR file" 2>&1 | tee -a $LOGFILE
 if [ ! -f "$WILDFLY_HOME/standalone/deployments/$NEW_EAR" ]; then 
 	# stop wildfly service
 	service wildfly stop
 	cp $install_root/packages/$NEW_EAR $WILDFLY_HOME/standalone/deployments/ 2>&1 | tee -a $LOGFILE
-	echo "STEP 4 file copied " 2>&1 | tee -a $LOGFILE
+	echo "STEP 4 end: file deployed " 2>&1 | tee -a $LOGFILE
 	# and restart it
 	service wildfly start
 else 
-	echo "STEP 4 not copied" 2>&1 | tee -a $LOGFILE
-	echo "Deployment übersprungen, da die Datei bereits in $WILDFLY_HOME/standalone/deployments/ vorhanden ist."
+	# Deployment übersprungen, da die Datei bereits in $WILDFLY_HOME/standalone/deployments/ vorhanden ist.
+	echo "STEP 4 fail: file not deployed" 2>&1 | tee -a $LOGFILE
 fi
+
 echo ""
-echo "++++++++++++++++++++++"
+echo "++++++++++++++++++++++"  2>&1 | tee -a $LOGFILE
 echo ""
 
 # STEP 5 - create new mail service
-echo "SMTP Configuration"
+echo "STEP 5 SMTP Configuration"
 . ./smtp_setup_config.sh
+echo "STEP 5 end: SMTP configured " 2>&1 | tee -a $LOGFILE
 
 echo ""
 echo "++++++++++++++++++++++"  2>&1 | tee -a $LOGFILE
