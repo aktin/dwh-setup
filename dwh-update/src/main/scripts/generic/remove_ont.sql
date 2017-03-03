543506fae49324522a869fd3906e16b7cbe9e0c2
Volker Thiemann <volker.thiemann@uni-oldenburg.de>
2017-03-03 16:28:30 +0100

    Formatierung

--- dwh-update/src/main/scripts/generic/remove_ont.sql
+++ dwh-update/src/main/scripts/generic/remove_ont.sql
@@ -1,6 +1,6 @@
 DELETE FROM i2b2metadata.table_access WHERE c_table_cd LIKE 'test%';
 DELETE FROM i2b2metadata.i2b2 WHERE sourcesystem_cd='test';
-DELETE FROM i2b2crcdata.concept_dimension WHERE sourcesystem_cd='test'
-;DELETE FROM i2b2metadata.table_access WHERE c_table_cd LIKE 'aktin%';
+DELETE FROM i2b2crcdata.concept_dimension WHERE sourcesystem_cd='test';
+DELETE FROM i2b2metadata.table_access WHERE c_table_cd LIKE 'aktin%';
 DELETE FROM i2b2metadata.i2b2 WHERE sourcesystem_cd='aktin';
 DELETE FROM i2b2crcdata.concept_dimension WHERE sourcesystem_cd='aktin';
