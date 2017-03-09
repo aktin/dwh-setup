509c3cc6fe436ced6f1e03b4b1fb7feb291e584c
Tingyan Xu <tingyan.xu@uni-oldenburg.de>
2017-03-09 14:58:15 +0100

    update script push

--- dwh-update/src/main/scripts/generic/aktin_dwh_update.sh
+++ dwh-update/src/main/scripts/generic/aktin_dwh_update.sh
@@ -31,14 +31,14 @@ OS_VERSION=debian
 # debian is standard
 # if [ $(ls /etc/d* | grep -c "debian") -gt 1 ] ; then 
     WILDFLY_START=service wildfly start
-    WILDFLY_STOP=service wildlfy stop
+    WILDFLY_STOP=service wildfly stop
     WILDFLY_STATUS=service wildfly status
 # fi
 # $(ls /etc/c* | grep -c "centos") -gt 1
 if [ -f "/etc/centos-release" ] ; then 
     OS_VERSION=centos
     WILDFLY_START=systemctl start wildfly
-    WILDFLY_STOP=systemctl stop wildlfy 
+    WILDFLY_STOP=systemctl stop wildfly 
     WILDFLY_STATUS=systemctl status wildfly 
 fi
 echo System als $OS_VERSION erkannt
