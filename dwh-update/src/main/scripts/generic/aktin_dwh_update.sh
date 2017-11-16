25ba1c2461cae34c863da6de926ba3140fefa3d7
Tingyan Xu <tingyan.xu@uni-oldenburg.de>
2017-11-16 13:57:51 +0100

    update: moved 'remove cdatmp folder' to later

--- dwh-update/src/main/scripts/generic/aktin_dwh_update.sh
+++ dwh-update/src/main/scripts/generic/aktin_dwh_update.sh
@@ -187,7 +187,6 @@ su - postgres bash -c "psql -d i2b2 -f $CDATMPDIR/sql/meta.sql" 2>&1 >> $SQLLOG
 echo "-- update crcdata" 2>&1 | tee -a $LOGFILE | tee -a $SQLLOG
 su - postgres bash -c "psql -d i2b2 -f $CDATMPDIR/sql/data.sql" 2>&1 >> $SQLLOG
 # remove temp directory
-rm -r $CDATMPDIR
 echo "- Ontology Update done. Result logged in $SQLLOG"
 
 
@@ -197,6 +196,13 @@ echo
 cp -v $INSTALL_ROOT/lib/fix_visit_patient_date_accuracy.sql $CDATMPDIR/sql/fix_visit_patient_date_accuracy.sql 2>&1 | tee -a $LOGFILE  # copy the remove ont file 
 echo "-- fix visit patient date accuracy" 2>&1 | tee -a $LOGFILE | tee -a $SQLLOG
 su - postgres bash -c "psql -d i2b2 -f $CDATMPDIR/sql/fix_visit_patient_date_accuracy.sql" 2>&1 >> $SQLLOG
+
+
+echo
+echo +++++ STEP 2.02.end +++++ Aufräumen der Temporären Dateien | tee -a $LOGFILE
+echo
+echo "- löschen des Ordners"
+rm -r $CDATMPDIR
 
 echo
 echo +++++ STEP 2.03 +++++ Entfernen der Defaulteinträge in Loginformular | tee -a $LOGFILE
