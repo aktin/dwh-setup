548c32556fce4aecab8be20214532c8a92b1b407
Alexander Kombeiz <akombeiz@ukaachen.de>
2020-10-15 15:39:34 +0200

    removed all old installation files

--- dwh-install/src/main/resources/i2b2_install/db_hive/scripts/drop_hive_tables.sql
+++
@@ -1,8 +0,0 @@
-
---------------------------------------------------------
---  REMOVE HIVE TABLES
---------------------------------------------------------
- 
- DROP TABLE IF EXISTS ONT_DB_LOOKUP, IM_DB_LOOKUP, WORK_DB_LOOKUP, CRC_DB_LOOKUP, CRC_ANALYSIS_JOB  CASCADE;
-
- 
--- dwh-install/src/main/resources/sql/i2b2_maintenance.sql
+++
@@ -1,5 +0,0 @@
--- This maintenance script should be run in periodic intervals. e.g. daily/weekly
--- update patient age column
-UPDATE patient_dimension SET age_in_years_num = DATE_PART('year',AGE(birth_date));
-
--- update death date and vital status from AKTIN data set
