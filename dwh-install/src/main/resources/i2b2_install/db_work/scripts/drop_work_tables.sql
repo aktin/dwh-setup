145f9dcf760ebdca3f3dda116ea87015b78ae20a
Alexander Kombeiz <akombeiz@ukaachen.de>
2020-10-15 15:39:34 +0200

    removed all old installation files

--- dwh-install/src/main/resources/i2b2_install/db_work/scripts/drop_work_tables.sql
+++
@@ -1,8 +0,0 @@
-
---------------------------------------------------------
---  REMOVE WORK TABLES
---------------------------------------------------------
- 
- DROP TABLE IF EXISTS WORKPLACE, WORKPLACE_ACCESS  CASCADE;
-
- 
