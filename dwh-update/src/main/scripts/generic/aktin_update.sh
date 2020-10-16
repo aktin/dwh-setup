310b1e2f039897e381a7009e6007d79562cee7c6
Alexander Kombeiz <akombeiz@ukaachen.de>
2020-10-16 13:25:47 +0200

    moved webclient and wildfly download further down, added i2b2.war download, small fixes

--- dwh-update/src/main/scripts/generic/aktin_update.sh
+++ dwh-update/src/main/scripts/generic/aktin_update.sh
@@ -2,13 +2,14 @@
 
 # script to install aktin-update on ubuntu 20.04
 # maintainer: Alexander Kombeiz <akombeiz@ukaachen.de>
-# september 2020
+set -euo pipefail # stop on errors
 
 readonly AKTIN_VERSION=${dwhJ2EEVersion}
 
 readonly INSTALL_ROOT=${path.install.link}
-readonly SQL_FILES=$INSTALL_ROOT/sql
-readonly PACKAGES=$INSTALL_ROOT/packages
+readonly INSTALL_DEST=${path.install.destination}
+readonly UPDATE_ROOT=$(pwd)
+readonly UPDATE_PACKAGES=$UPDATE_ROOT/packages
 
 readonly WILDFLY_HOME=$INSTALL_DEST/wildfly
 
@@ -21,7 +22,7 @@ readonly GRE=${color.green}
 
 
 # stop wildfly server safely
-cd $SCRIPT_FILES
+cd $INSTALL_ROOT
 ./wildfly_safe_stop.sh
 
 # remove all old dwh-j2ee.ears
@@ -30,16 +31,17 @@ if [[ -f $WILDFLY_HOME/standalone/deployments/dwh-j2ee-*.ear ]]; then
 	rm $WILDFLY_HOME/standalone/deployments/dwh-j2ee-*
 else
 	echo -e "${ORA}In $WILDFLY_HOME/standalone/deployments sind keine dwh-j2ee.ear vorhanden.${WHI}"
+fi
 
 # deploy aktin ear and give permissions to wildfly user
 if [[ ! -f $WILDFLY_HOME/standalone/deployments/dwh-j2ee-$AKTIN_VERSION.ear ]]; then
 	echo -e "${YEL}dwh-j2ee-$AKTIN_VERSION.ear wird nach $WILDFLY_HOME/standalone/deployments verschoben.${WHI}"
-	cp $PACKAGES/dwh-j2ee-$AKTIN_VERSION.ear $WILDFLY_HOME/standalone/deployments/
+	cp $UPDATE_PACKAGES/dwh-j2ee-$AKTIN_VERSION.ear $WILDFLY_HOME/standalone/deployments/
 	chown wildfly:wildfly $WILDFLY_HOME/standalone/deployments/dwh-j2ee-$AKTIN_VERSION.ear
 else
 	echo -e "${ORA}dwh-j2ee-$AKTIN_VERSION.ear ist bereits in $WILDFLY_HOME/standalone/deployments vorhanden.${WHI}"
 fi
 
 # start wildfly server safely
-cd $SCRIPT_FILES
+cd $INSTALL_ROOT
 ./wildfly_safe_start.sh
