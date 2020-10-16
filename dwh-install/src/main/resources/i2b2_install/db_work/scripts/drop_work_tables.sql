03a655a17bfb1d0932f65d41244bd5fad9d156ee
Alexander Kombeiz <akombeiz@ukaachen.de>
2020-10-16 07:43:35 +0200

    Revert "removed all old installation files"
    This reverts commit 548c32556fce4aecab8be20214532c8a92b1b407.

---
+++ dwh-install/src/main/resources/i2b2_install/db_work/scripts/drop_work_tables.sql
@@ -0,0 +1,8 @@
+
+--------------------------------------------------------
+--  REMOVE WORK TABLES
+--------------------------------------------------------
+ 
+ DROP TABLE IF EXISTS WORKPLACE, WORKPLACE_ACCESS  CASCADE;
+
+ 
