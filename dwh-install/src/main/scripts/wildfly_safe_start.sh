2d018d3f012fee039211ccb9aa600dbe057d092d
Alexander Kombeiz <akombeiz@ukaachen.de>
2020-10-19 12:43:59 +0200

    reworked changes of jboss cli in safe_start/stop, because undeployment needs to happen before wildfly start

--- dwh-install/src/main/scripts/wildfly_safe_start.sh
+++ dwh-install/src/main/scripts/wildfly_safe_start.sh
@@ -3,9 +3,7 @@
 # script to safely start wildfly
 set -euo pipefail # stop on errors
 
-readonly WILDFLY_HOME=${path.wildfly.link}
-readonly JBOSSCLI="$WILDFLY_HOME/bin/jboss-cli.sh -c"
-readonly WILDFLY_DEPLOYMENTS=$WILDFLY_HOME/standalone/deployments
+readonly WILDFLY_DEPLOYMENTS=${path.wildfly.link}/standalone/deployments
 
 
 # start wildfly if not running
@@ -13,7 +11,10 @@ if  [[ $(service wildfly status | grep "not" | wc -l) == 1 ]]; then
 
 	# if postgresql is not running: undeploy dwh.ear prior starting to prevent crash 
 	if  [[ $(service postgresql status | grep "down" | wc -l) == 1 ]]; then
-		$JBOSSCLI --command="undeploy --name=dwh-j2ee*.ear"
+		for i in $(ls $WILDFLY_DEPLOYMENTS | grep "dwh-j2ee-*.");
+			do
+				mv "$WILDFLY_DEPLOYMENTS/$i" "$WILDFLY_DEPLOYMENTS/$i.UNDEPLOYED";
+			done
 		service wildfly start
 	else	
 		service wildfly start
--- dwh-install/src/main/scripts/wildfly_safe_stop.sh
+++ dwh-install/src/main/scripts/wildfly_safe_stop.sh
@@ -3,9 +3,7 @@
 # script to stop wildfly after safely starting it
 set -euo pipefail # stop on errors
 
-readonly WILDFLY_HOME=${path.wildfly.link}
-readonly JBOSSCLI="$WILDFLY_HOME/bin/jboss-cli.sh -c"
-readonly WILDFLY_DEPLOYMENTS=$WILDFLY_HOME/standalone/deployments
+readonly WILDFLY_DEPLOYMENTS=${path.wildfly.link}/standalone/deployments
 
 
 # stop wildfly if running
@@ -13,7 +11,9 @@ if  [[ $(service wildfly status | grep "not" | wc -l) == 0 ]]; then
 	service wildfly stop
 fi
 
-# remove undeployed .ear through wildfly_safe_start.sh
-if [[ -n $(ls $WILDFLY_DEPLOYMENTS | grep ".ear.undeployed") ]]; then
-	rm $WILDFLY_DEPLOYMENTS/*.ear.undeployed
-fi
+# redeploy undeployed .ear of wildfly_safe_start.sh
+for i in $(ls $WILDFLY_DEPLOYMENTS | grep ".UNDEPLOYED");
+	do 
+		mv "$WILDFLY_DEPLOYMENTS/$i" "$WILDFLY_DEPLOYMENTS/${i%.UNDEPLOYED}"; 
+	done
+
