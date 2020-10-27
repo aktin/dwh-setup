7d1ab855d4f7002181a8f0bd227417f0217f9873
Alexander Kombeiz <akombeiz@ukaachen.de>
2020-10-27 07:38:36 +0100

    added python3-gunicorn to installer and install-manual

--- dwh-install/src/main/scripts/aktin_install.sh
+++ dwh-install/src/main/scripts/aktin_install.sh
@@ -69,7 +69,7 @@ postgresql-$PG_VERSION \
 apache2=$APACHE2_VERSION \
 php$PHP_VERSION php$PHP_VERSION-common libapache2-mod-php$PHP_VERSION php$PHP_VERSION-curl \
 r-base-core=$R_VERSION r-cran-lattice r-cran-xml \
-python$PYTHON_VERSION python3-pandas python3-numpy python3-requests python3-sqlalchemy python3-psycopg2 python3-postgresql python3-zipp python3-plotly python3-unicodecsv
+python$PYTHON_VERSION python3-pandas python3-numpy python3-requests python3-sqlalchemy python3-psycopg2 python3-postgresql python3-zipp python3-plotly python3-unicodecsv python3-gunicorn
 }
 
 
