3fc9a136350d4a3c8f5f1b674627b9a61d26f680
Alexander Kombeiz <akombeiz@ukaachen.de>
2020-11-05 11:06:10 +0100

    updated integration test for study AKTIN

--- dwh-integration-test/src/main/scripts/test_postgresql_background_validation.sh
+++ dwh-integration-test/src/main/scripts/test_postgresql_background_validation.sh
@@ -37,12 +37,12 @@ done
 BEARER_TOKEN=$(curl -s --location --request POST 'http://localhost:80/aktin/admin/rest/auth/login/' --header 'Content-Type: application/json' --data-raw '{ "username": "i2b2", "password": "demouser" }')
 RANDOM_STRING=$(echo $(cat /dev/urandom | tr -dc 'a-zA-Z' | fold -w 6 | head -n 1))
 RANDOM_NUMBER=$(echo $(cat /dev/urandom | tr -dc '0-9' | fold -w 6 | head -n 1))
-RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" --location --request POST 'http://localhost:80/aktin/admin/rest/optin/ENQUIRE/Patient/1.2.276.0.76.4.8/'$RANDOM_NUMBER'' --header 'Authorization: Bearer '$BEARER_TOKEN'' --header 'Content-Type: application/json' --data-raw '{ "opt": 0, "sic": "", "comment": "'$RANDOM_STRING'" }')
+RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" --location --request POST 'http://localhost:80/aktin/admin/rest/optin/AKTIN/Patient/1.2.276.0.76.4.8/'$RANDOM_NUMBER'' --header 'Authorization: Bearer '$BEARER_TOKEN'' --header 'Content-Type: application/json' --data-raw '{ "opt": 0, "sic": "", "comment": "'$RANDOM_STRING'" }')
 if [[ $RESPONSE_CODE == 400 || $RESPONSE_CODE == 401 ]]; then
 	echo -e "${GRE}Test consent-manager successfully failed ($RESPONSE_CODE)${WHI}"
 else
 	echo -e "${RED}Test consent-manager ($RESPONSE_CODE)${WHI}"
-	echo $(curl -s --location --request POST 'http://localhost:80/aktin/admin/rest/optin/ENQUIRE/Patient/1.2.276.0.76.4.8/'$RANDOM_NUMBER'' --header 'Authorization: Bearer '$BEARER_TOKEN'' --header 'Content-Type: application/json' --data-raw '{ "opt": 0, "sic": "", "comment": "'$RANDOM_STRING'" }')
+	echo $(curl -s --location --request POST 'http://localhost:80/aktin/admin/rest/optin/AKTIN/Patient/1.2.276.0.76.4.8/'$RANDOM_NUMBER'' --header 'Authorization: Bearer '$BEARER_TOKEN'' --header 'Content-Type: application/json' --data-raw '{ "opt": 0, "sic": "", "comment": "'$RANDOM_STRING'" }')
 fi
 echo
 
